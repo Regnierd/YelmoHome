a077472e5b2086e71cc942ada1ce35afdfd9ce08
regnierd <javivi_lp_96@hotmail.es>
2022-05-05 10:31:50 +0100

    fix video & description

--- src/app/components/film-player/film-player.component.ts
+++ src/app/components/film-player/film-player.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { Category } from 'src/app/data/category';
 import { Film } from 'src/app/data/pelicula';
@@ -12,11 +13,13 @@ import { ConnectionServerService } from 'src/app/services/connection-server.serv
 })
 export class FilmPlayerComponent implements OnInit {
 
-  constructor(public connectionServerService:ConnectionServerService, private route: ActivatedRoute) { }
+  constructor(public connectionServerService:ConnectionServerService, private route: ActivatedRoute, private domSanitizer: DomSanitizer) { }
 
   categories: Category[] = this.connectionServerService.category
   selectedFilm: Film = this.connectionServerService.selectedFilm;
-  urlFilm: string = this.selectedFilm.video;
+  urlFilm: SafeResourceUrl = this.domSanitizer.bypassSecurityTrustResourceUrl("");
+  
+//  urlFilm: string = this.selectedFilm.video;
   expandirTexto:boolean = false;
 
   ngOnInit(): void {
@@ -27,6 +30,7 @@ export class FilmPlayerComponent implements OnInit {
   ngDoCheck(): void{
     this.getPelicula();
     
+    console.log(this.selectedFilm.video);
   }
 
   /**
@@ -35,7 +39,10 @@ export class FilmPlayerComponent implements OnInit {
    getPelicula(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.connectionServerService.getPelicula(id)
-      .subscribe(film => this.selectedFilm = film);
+      .subscribe(film => { 
+        this.selectedFilm = film;
+        this.urlFilm = this.domSanitizer.bypassSecurityTrustResourceUrl(this.selectedFilm.video);
+      });
   }
 
 
