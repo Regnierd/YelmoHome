8633c6255e1694df84169c101537981794ac03fb
regnierd <javivi_lp_96@hotmail.es>
2022-05-07 11:26:01 +0100

    fix video

--- src/app/components/film-player/film-player.module.ts
+++ src/app/components/film-player/film-player.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { FilmPlayerComponent } from './film-player.component';
 import { SafePipe } from 'src/app/pipes/safe.pipe';
+import { HeaderModule } from '../header/header.module';
 
 
 @NgModule({
   declarations: [FilmPlayerComponent, SafePipe],
   imports: [
     CommonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeaderModule
   ],
   exports:[FilmPlayerComponent]
 })
